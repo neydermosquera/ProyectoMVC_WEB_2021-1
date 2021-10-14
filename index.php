79a924cfbef08c0d7578ea671df4ad69f5ceee5a
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-14 17:50:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,30 @@
 </head>
 <body>
 
+    <!--Barra de navegación!-->
+
+    <nav class="navbar navbar-expand-lg barranavegacion">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SPA WEB</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Registro</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Datos</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
 
     
 </body>
