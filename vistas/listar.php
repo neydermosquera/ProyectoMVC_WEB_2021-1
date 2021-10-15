29e79b3e14784d6269fd74204707a17391e6103c
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-15 15:16:51 -0500

    Update listar.php

---
+++ vistas/listar.php
@@ -0,0 +1,52 @@
+<div class="container-fluid contenedorlistado" data-namespace="registro">
+    
+                    <div class="contenido">
+                        <h2>Datos registrados</h2> <br><br>
+                        <div class="row">
+                        </div><!--Cierre botones-->
+
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Nombres</th>
+                                <th>Apellidos</th>
+                                <th>T.D</th>
+                                <th>Documento</th>
+                                <th>Dirección</th>
+                                <th>Ciudad</th>
+                                <th>Departamento</th>
+                                <th>Fecha Nacimiento</th>
+                                <th>Ciudad Natal</th>
+                                <th>Depto Natal</th>
+                                <th>Email</th>
+                                <th>Telefono</th>
+                                <th>Usuario</th>
+                                <th>Acción</th>
+                            </thead>
+                            <tbody id="respuesta">
+                                <tr>
+                                <td>Nombres</td>
+                                <td>Apellidos</td>
+                                <td>T.D</td>
+                                <td>Documento</td>
+                                <td>Dirección</td>
+                                <td>Ciudad</td>
+                                <td>Departamento</td>
+                                <td>Fecha Nacimiento</td>
+                                <td>Ciudad Natal</td>
+                                <td>Depto Natal</td>
+                                <td>Email</td>
+                                <td>Telefono</td>
+                                <td>Usuario</td>
+                                <td>Acción</td>
+                               </tr> 
+                            </tbody>
+
+                        </table> <br><br>
+
+                        <div class="col-sm-12 text-center">
+                            <button type="button" class="btn btn-danger botonvolver" onclick="location.href='index.html'">Volver</button>
+                        </div> 
+
+                    </div>
+
+    </div>
