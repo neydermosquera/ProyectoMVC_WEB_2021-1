50638249d523bc1eadddfe96423951bcc5c7dd9b
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-16 21:14:04 -0500

    Create registrar.php

---
+++ vistas/registrar.php
@@ -0,0 +1,45 @@
+<?php
+
+    $user = "root";
+    $password = "";
+    $server = "localhost";
+    $db = "registro";
+
+    $conexion = new mysqli($server, $user, $password, $db);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $tipodocumento = $_POST['tipodocumento'];
+    $documento = $_POST['documento'];
+    $birthday = $_POST['birthday'];
+    $departamentonacimiento = $_POST['departamentonacimiento'];
+    $ciudadnacimiento = $_POST['ciudadnacimiento'];
+    $address = $_POST['address'];
+    $departamentoresidencia = $_POST['departamentoresidencia'];
+    $ciudadresidencia = $_POST['ciudadresidencia'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+
+    var_dump($nombres);
+    echo $nombres .'Welcome, your email adress is: '. $email;
+
+    $insertar = "INSERT INTO persona (nombres, apellidos, idtipodocumento, documento, direccion, idciudad, iddepartamento, fechanacimiento, iddepartamentonatal, idciudadnatal, email, telefono, usuario, password) 
+    VALUES ('$nombres','$apellidos','$tipodocumento','$documento','$address','$ciudadresidencia','$departamentoresidencia','$birthday','$departamentonacimiento','$ciudadnacimiento ','$email','$telefono','$usuario','$password')";
+        
+    $resultado = mysqli_query($conexion, $insertar);
+
+    if($resultado){
+
+        echo 'Registro Exitoso';
+    }else{
+        echo 'incorrecto';
+    }
+} else{
+    '<script> location.replace("index.html")';
+}
+?>
+
+
