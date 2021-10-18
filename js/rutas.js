5e594017a01c427b6fca6bed99d7758837310960
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-17 20:32:03 -0500

    Update rutas.js

--- js/rutas.js
+++ js/rutas.js
@@ -4,5 +4,10 @@
             .ruta('/', 'vistas/inicio.html', null, null)
             .ruta('/registrar-usuario', 'vistas/registro.php', null, null)
             .ruta('/listar-usuarios', 'vistas/listar.php', null, null)
-            .ruta('/actualizar-usuario', 'vistas/actualizar.php', null, null);
+            .ruta('/actualizar-usuario', 'vistas/actualizar.php', null, null)
+            .ruta('/editar-usuario', 'vistas/editar.php', null, null);
 })(window, document);
+
+if (window.history.replaceState) { 
+    window.history.replaceState(null, null, window.location.href);
+}
