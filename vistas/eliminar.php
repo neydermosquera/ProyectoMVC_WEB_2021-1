8d9fe293c650e98b3c91030b97fd72de5a896ff2
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-17 20:32:11 -0500

    Create eliminar.php

---
+++ vistas/eliminar.php
@@ -0,0 +1,14 @@
+<?php
+
+    $user = "root";
+    $password = "";
+    $server = "localhost";
+    $db = "registro";
+
+    $conexion = new mysqli($server, $user, $password, $db);
+
+    
+
+
+    
+?>
