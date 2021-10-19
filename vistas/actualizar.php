7e8c8c1592b922f1ab396cd0d6196ce882b09280
Neyder Mosquera <neyder2182078@correo.uis.edu.co>
2021-10-19 10:44:34 -0500

    Update actualizar.php

--- vistas/actualizar.php
+++ vistas/actualizar.php
@@ -1,156 +1,85 @@
-<!--Div Actualizar!-->
+<?php
 
-<div class="container contenedorregistro" data-namespace="registro">
-        <section class="container col-sm-11 principal 
-        margin-left: 10px;
-        margin-right: 10px;
-        ">
-            <br>
-            <div class="p-1 mb-1  text-white title">
-            <h3 class=" text-center">Registro de Usuario</h3>
-            </div>
-         <br>
-         <div class="container">
-            <form action="" method="POST" id = "formulario" name="formulario" class="formulario">
-            <div class="row">
+    $user = "root";
+    $password = "";
+    $server = "localhost";
+    $db = "registro";
 
-                    <div  class="form-group has-feedback col-sm-6">
-                        <i class="fa fa-user form-control-feedback"></i>
-                        <input type="text" class="form-control" placeholder="Nombres" id="nombres" name="nombres">
-                    </div>
-                    
-                    <div  class="form-group has-feedback col-sm-6">
-                        <i class="fa fa-user form-control-feedback"></i>
-                        <input type="text" class="form-control" placeholder="Apellidos" id="apellidos" name="apellidos">
-                    </div>
+    $conexion = new mysqli($server, $user, $password, $db);
 
-                    <div  class="form-group has-feedback col-sm-6">
-                        <select class="form-select" id="tipodocumento" name="tipodocumento">
-                            <option selected>Tipo de documento de Identidad</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                          </select>
-                    </div>
+    $iduser = $_POST['iduser']; 
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $tipodocumento = $_POST['tipodocumento'];
+    $documento = $_POST['documento'];
+    $birthday = $_POST['birthday'];
+    $departamentonacimiento = $_POST['departamentonacimiento'];
+    $ciudadnacimiento = $_POST['ciudadnacimiento'];
+    $address = $_POST['address'];
+    $departamentoresidencia = $_POST['departamentoresidencia'];
+    $ciudadresidencia = $_POST['ciudadresidencia'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
 
-                    <div  class="form-group has-feedback col-sm-6">
-                        <i class="fa fa-id-card-o form-control-feedback"></i>
-                        <input type="number" class="form-control" placeholder="Número de documento" id="documento" name="documento">
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-12">
-                        <p>Fecha y Lugar de nacimiento:</p>
-                        <i class="fa fa-birthday-cake form-control-feedback"></i>
-                        <input type="date" class="form-control" placeholder="Fecha de Nacimiento" id="birthday" name="birthday" value="">
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-6">
-                        <select class="form-select" id="departamentonacimiento" name="departamentonacimiento">
-                            <option selected>Departamento</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                          </select>
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-6">
-                        <select class="form-select" id="ciudadnacimiento" name="ciudadnacimiento">
-                            <option selected>Ciudad</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                          </select>
-                    </div> <br>
-
-
-                    <div  class="form-group has-feedback col-sm-12">
-                        <p>Lugar de residencia actual:</p>
-                        <i class="fa fa-map-marker  form-control-feedback"></i>
-                        <input type="text" class="form-control" placeholder="Dirección" id="address" name="address">
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-6">
-                        <select class="form-select" id="departamentoresidencia" name="departamentoresidencia">
-                            <option selected>Departamento</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                          </select>
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-6">
-                        <select class="form-select" id="ciudadresidencia" name="ciudadresidencia">
-                            <option selected>Ciudad</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                          </select>
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-12">
-                        <i class="fa fa-envelope form-control-feedback"></i>
-                        <input type="text" class="form-control" placeholder="Email" id="email" name="email">
-                    </div>
-
-                    <div  class="form-group has-feedback col-sm-12">
-                        <i class="fa fa-phone form-control-feedback"></i>
-                        <input type="number" class="form-control" placeholder="Número de Telefono" id="telefono" name="telefono">
-                    </div>
-
-                    <div  class="form-group formulario__grupo  has-feedback col-sm-6" id="grupo__usuario">
-                        <i class="fa fa-user form-control-feedback"></i>
-                        <div class="formulario__grupo-input">
-                            <input type="text" class="form-control" placeholder="Usuario" name = "usuario" id="usuario">
-                            <i class="fa fa-times-circle-o formulario__validacion-estado" id="icono"></i>
-                        </div>
-                        <p class="formulario__input-error">El nombre de usuario debe tener entre 5 a 15 caracteres.</p>
-                    </div>
-                    
-                    <div  class="form-group formulario__grupo  has-feedback col-sm-6" id="grupo__usuario2">
-                        <i class="fa fa-user form-control-feedback"></i>
-                        <div class="formulario__grupo-input">
-                            <input type="text" class="form-control" placeholder="Confirmar Usuario" name = "usuario2" id="usuario2">
-                            <i class="fa fa-times-circle-o formulario__validacion-estado" id="icono"></i>
-                        </div>
-                        <p class="formulario__input-error">El nombre de usuario no conincide.</p>
-                    </div>
     
-                    <div  class="form-group formulario__grupo  has-feedback col-sm-6" id="grupo__password">
-                        <i class="fa fa-lock form-control-feedback"></i>
-                        <div class="formulario__grupo-input">
-                            <input type="password" class="form-control" placeholder="Contraseña" id="password" name="password">
-                            <i class="fa fa-times-circle-o formulario__validacion-estado" id="icono"></i>
-                        </div>
-                        <p class="formulario__input-error">La contraseña debe tener entre 4 a 12 caracteres.</p>
-                    </div>
+    $actualizar = "UPDATE persona SET nombres = '$nombres', 
+    apellidos = '$apellidos', 
+    idtipodocumento = $tipodocumento, 
+    documento = $documento, 
+    direccion = '$address', 
+    idciudad = $ciudadresidencia, 
+    iddepartamento = $departamentoresidencia, 
+    fechanacimiento = '$birthday', 
+    iddepartamentonatal = $departamentonacimiento, 
+    idciudadnatal = $ciudadnacimiento, 
+    email = '$email', 
+    telefono = $telefono, 
+    usuario = '$usuario', 
+    password = '$password' 
+    WHERE id = $iduser;";
+        
+        $resultado = mysqli_query($conexion, $actualizar) or die(mysqli_error($conexion));
+
+       
     
-                    <div  class="form-group formulario__grupo  has-feedback col-sm-6" id="grupo__password2">
-                        <i class="fa fa-lock form-control-feedback"></i>
-                        <div class="formulario__grupo-input">
-                            <input type="password" class="form-control" placeholder=" Confirmar Contraseña" id="password2" name="password2">
-                            <i class="fa fa-times-circle-o formulario__validacion-estado" id="icono"></i>
-                        </div>
-                        <p class="formulario__input-error">Las contraseñas no coninciden.</p>
-                    </div>
-
-                    <div class="formulario__mensaje col-sm-12 text-center btn-danger" id="formulario__mensaje">
-                        <p><i class="fa fa-exclamation-triangle"></i> Error: <b>Complete correctamente el formulario de registro.</b></p>
-                    </div> <br> <br> <br>
-
-                    <div class="col-sm-6 text-center">
-                        <button type="button" class="btn btn-danger botonvolver" onclick="location.href='index.html'">Cancelar/Volver</button>
-                    </div> <br> <br> <br>
+        
     
-                    <div class="col-sm-6 text-center">
-                        <button type="submit" class="btn btn-success boton">Registrar</button>
-                    </div> <br> <br> <br>
+?>
 
-                    <div class="formulario__mensaje col-sm-12 text-center btn-success" id="formulario__mensaje">
-                        <p class="formulario__mensaje-exito"><i class="fa fa-check-circle"></i> <b> Registro Éxitoso.</b></p>
-                    </div> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-            </div>
-            </form>
-         </div>
-        </section>
-    </div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="../css/estilos.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+ 
+    
+    
+    <title>SPA Trabajo Final</title>
+</head>
+
+<body>
+<div class="container contenedorregistro" data-namespace="registro" style="height: 200px;">
+
+            <div class="p-1 mb-1  text-white  title bg-success">
+            <h3 class=" text-center">¡Actualización realizada con éxito! <i class="fa fa-check-square-o"></i> </h3>
+
+</div>
+
+<div class="col-sm-12 text-center divbotones">
+        <button type="button" class="btn botonvolver" onclick="location.href='../index.php#/listar-usuarios'">Volver</button>
+</div> <br> <br> <br>
+
+
+    
+</body>
+</html>
